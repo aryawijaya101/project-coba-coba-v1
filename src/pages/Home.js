48f62a30f6fba700c1953562352975f20ca06db3
aryawijaya101 <arya01844@gmail.com>
2021-11-25 20:36:17 +0700

    empat commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
     <div className="home" style={{ backgroundImage: `url(${Pizza})` }}>
       <div className="headerContainer">
         <h1> PIZZA OF THE YEAR</h1>
-        <p>PIZZA ARYA WIJYA V2</p>
+        <p>PIZZA ARYA WIJYA V3</p>
         <Link to="/menu">
           <button>ORDER</button>
         </Link>

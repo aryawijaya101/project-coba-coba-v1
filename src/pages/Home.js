12976fca9694eb383b6be39a4e6dcc25d325fdf1
aryawijaya101 <arya01844@gmail.com>
2021-11-25 20:31:06 +0700

    tiga commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
     <div className="home" style={{ backgroundImage: `url(${Pizza})` }}>
       <div className="headerContainer">
         <h1> PIZZA OF THE YEAR</h1>
-        <p>PIZZA ARYA WIJYA</p>
+        <p>PIZZA ARYA WIJYA V2</p>
         <Link to="/menu">
           <button>ORDER</button>
         </Link>

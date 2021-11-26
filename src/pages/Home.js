853e92ddced435c764cd8711057a4a432f17c0df
aryawijaya101 <arya01844@gmail.com>
2021-11-26 17:42:27 +0700

    coba commit v1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
     <div className="home" style={{ backgroundImage: `url(${Pizza})` }}>
       <div className="headerContainer">
         <h1> BEST PIZZA</h1>
-        <p>BY ARYA WIJAYA</p>
+        <p>BY ARYA WIJAYA V1</p>
         <Link to="/menu">
           <button>ORDER</button>
         </Link>
